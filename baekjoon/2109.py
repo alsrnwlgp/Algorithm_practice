1ff5e613bfea3638072bd054eecbfb402e18224f
mingu <alsrnwlgp@dgist.ac.kr>
2021-11-04 00:13:45 +0900

    211104

--- baekjoon/2109.py
+++ baekjoon/2109.py
@@ -5,11 +5,8 @@ for _ in range(n):
   works.append(tuple(map(int, input().split())))
 works = sorted(works, key=lambda x: x[1])
 sums = []
-preday = works[0][1]
 for pay, day in works:
-  if preday != day:
-    while preday < len(sums):
-      heapq.heappop(sums)
   heapq.heappush(sums, pay)
-  preday = day
+  if day < len(sums):
+      heapq.heappop(sums)
 print(sum(sums))
